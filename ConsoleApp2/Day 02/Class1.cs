0bd0f7cc0a7374b4b658f7c48a49adb2523292f5
mai0809 <Maitrayee.Kar@fnf.com>
2021-09-07 14:23:56 +0530

    Add project files.

---
+++ ConsoleApp2/Day 02/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Day_02
+{
+    class Class1
+    {
+        static bool checkPrime(int numberToCheck)
+        {
+            if (numberToCheck == 1)
+            {
+                return false;
+            }
+            for (int i = 2;
+                     i * i <= numberToCheck; i++)
+            {
+                if (numberToCheck % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+  
+        static int primeSum(int l, int r)
+        {
+            int sum = 0;
+            for (int i = r; i >= l; i--)
+            {
+
+                // Check for prime
+                bool isPrime = checkPrime(i);
+                if (isPrime)
+                {
+
+                    // Sum the prime number
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+        
+        public static void Main()
+        {
+            int l = 4, r = 13;
+
+            
+            Console.Write(primeSum(l, r));
+        }
+    }
+
+
+
+
+
+
+}
---
+++ ConsoleApp2/Day 02/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Day_02
+{
+    class Class2
+    {
+        public static int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+        public static void main(string[] args)
+        {
+            int n = 9;
+            Console.Write(Fib(n));
+        }
+    }
+}
